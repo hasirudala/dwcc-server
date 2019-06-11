cfc5f1407fc7436c96bc839e1749deb977470ce0
Vivek Singh <petmongrels@gmail.com>
2019-06-11 19:04:10 +0530

    HomeController working with test. INcluding few .idea files as it is being recommended by intellij (seems like it can be shared)

---
+++ src/main/java/org/hasirudala/buguri/server/web/HelloController.java
@@ -0,0 +1,12 @@
+package org.hasirudala.buguri.server.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class HelloController {
+    @GetMapping
+    public String helloGradle() {
+        return "Hello World!";
+    }
+}
